5508282fc9b37b71e8de597a86b3ab1dd87f3d6b
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2026-01-21 10:15:35 +0100

    feat: ajouter le composant SearchBar pour la recherche de villes

--- meteo-app/src/App.jsx
+++ meteo-app/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
-import Weather from './components/Weather/Weather'
+import Weather from './components/Weather/Weather';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   console.log("Ma clé API :", import.meta.env.VITE_WEATHER_API_KEY)
   return (
     <div className='App'>
       <h1>Météo App</h1>
+      <Weather />
     </div>
   )
 
---
+++ meteo-app/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const SearchBar = ({ onSearch }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (inputValue.trim !== '') {
+            onSearch(inputValue);
+            setInputValue('');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="search-bar">
+            <input
+                type="text"
+                placeholder="Entrez la ville"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button type="submit">Rechercher</button>
+        </form>
+    );
+};
+
+export default SearchBar;
